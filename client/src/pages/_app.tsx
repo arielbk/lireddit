dc9b21541f97d3b68ef9781938928ffcf8ca2367
arielbk <buchwald.kearney@gmail.com>
2020-11-20 17:14:04 +0100

    Add urql custom cache update

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,61 @@
-import { createClient, Provider } from 'urql';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
+
+// helper function for URQL types (which suck)
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query,
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:3030/graphql',
+  // force update cache when user logs in or registers
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => {
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache,
+            {query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.login.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.login.user,
+                }
+              }
+            }
+          )
+        },
+        register: (_result, args, cache, info) => {
+          betterUpdateQuery<RegisterMutation, MeQuery>(
+            cache,
+            {query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.register.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.register.user,
+                }
+              }
+            }
+          )
+        },
+      }
+    }
+  }), fetchExchange],
   fetchOptions: {
     credentials: "include"
   }
@@ -12,7 +63,7 @@ const client = createClient({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider value={client}>
+  <Provider value={client}>
     <ChakraProvider theme={theme}>
         <Component {...pageProps} />
     </ChakraProvider>
